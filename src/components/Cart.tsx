8d4cbe77561740332e12996a07da2d102db13cbd
Kita Igniter <smtecsystem.469@gmail.com>
2022-12-23 09:14:59 -0300

    Changing States

---
+++ src/components/Cart.tsx
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+
+export default function Cart () {
+  const state = useSelector(state => state);
+
+  console.log(state);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Produto</th>
+          <th>Preço</th>
+          <th>Quantidade</th>
+          <th>Subtotal</th>
+        </tr>
+      </thead>
+      <tbody>
+        
+      </tbody>
+    </table>
+  );
+}
