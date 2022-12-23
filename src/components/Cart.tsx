6a6f263e073cd533eb535de8f4c3cb9214e7214d
Kita Igniter <smtecsystem.469@gmail.com>
2022-12-23 09:27:59 -0300

    Redux Types

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,9 +1,9 @@
 import { useSelector } from "react-redux";
+import { IState } from "../store";
+import { ICartItem } from "../store/modules/cart/types";
 
 export default function Cart () {
-  const state = useSelector(state => state);
-
-  console.log(state);
+  const cart = useSelector<IState, ICartItem[]>(state => state.cart.items);
 
   return (
     <table>
@@ -16,7 +16,14 @@ export default function Cart () {
         </tr>
       </thead>
       <tbody>
-        
+        {cart.map(item => (
+          <tr key={item.product.id}>
+            <td>{item.product.title}</td>
+            <td>{item.product.price}</td>
+            <td>{item.quantity}</td>
+            <td>{(item.product.price * item.quantity).toFixed(2)}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,6 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./modules/rootReducer";
 
+import { ICartState } from "./modules/cart/types";
+
+export interface IState {
+  cart: ICartState;
+};
+
 const store = configureStore({
   reducer: rootReducer
 });
